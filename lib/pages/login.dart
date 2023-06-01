b0b5945b09583c11ddcaac6fc1e223bce35973d8
alanbacont <almabarocioco@ittepic.edu.mx>
2023-06-01 02:25:13 -0700

    A mimir

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -21,42 +21,51 @@ class _LoginState extends State<Login> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
+              const Text('Iniciar Sesión', style: TextStyle(fontSize: 30, fontFamily: "Caviar",)),
+              const SizedBox(height: 20.0),
               TextField(
                 controller: _userController,
                 decoration: const InputDecoration(labelText: 'Usuario'),
               ),
+              const SizedBox(height: 20.0),
               TextField(
                 controller: _passwordController,
                 decoration: const InputDecoration(labelText: 'Contraseña'),
                 obscureText: true,
               ),
-              ElevatedButton(
-                child: const Text('Iniciar Sesión'),
-                onPressed: () async {
-                  String _user = _userController.text;
-                  String _password = _passwordController.text;
+              const SizedBox(height: 20.0),
+              Container(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  child: const Text('Iniciar Sesión'),
+                  onPressed: () async {
+                    String _user = _userController.text;
+                    String _password = _passwordController.text;
 
-                  if (_user.isEmpty || _password.isEmpty) {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text(
-                            'Los campos de usuario y contraseña no pueden estar vacíos.')));
-                  } else {
-                    try {
-                      var user = await loginUser(_user, _password);
-                      if (user) {
-                        Navigator.popAndPushNamed(context, "/home");
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text('Error al iniciar sesión.')));
-                      }
-                    } catch (e) {
+                    if (_user.isEmpty || _password.isEmpty) {
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          content: Text('Error al iniciar sesión.')));
+                          content: Text(
+                              'Los campos de usuario y contraseña no pueden estar vacíos.')));
+                    } else {
+                      try {
+                        var user = await loginUser(_user, _password);
+                        if (user) {
+                          Navigator.popAndPushNamed(context, "/home");
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Error al iniciar sesión.')));
+                        }
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                            content: Text('Error al iniciar sesión.')));
+                      }
                     }
-                  }
-                },
+                  },
+                ),
               ),
+              const SizedBox(height: 20.0),
               TextButton(
                 child: const Text('¿No tienes una cuenta? Regístrate'),
                 onPressed: () async {
--- lib/pages/registro.dart
+++ lib/pages/registro.dart
@@ -1,5 +1,6 @@
 import 'package:dam_u5_proyecto/services/firebase_services.dart';
 import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -17,17 +18,26 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.popAndPushNamed(context, "/login"),
+          iconSize: 32,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: Center(
         child: Container(
           padding: EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text('Registro', style: TextStyle(fontSize: 30)),
+              const Text('Registro', style: TextStyle(fontSize: 30, fontFamily: "Caviar",)),
               const SizedBox(height: 20.0),
               TextField(
                 controller: _nombreController,
-                decoration: InputDecoration(labelText: 'Nombre'),
+                decoration: InputDecoration(labelText: 'Nombre', ),
               ),
               const SizedBox(height: 10.0),
               TextField(
@@ -48,20 +58,50 @@ class _SignUpState extends State<SignUp> {
               const SizedBox(height: 20.0),
               ElevatedButton(
                 child: const Text('Registrarse'),
-                onPressed: () async {
+                onPressed: () {
                   final String nombre = _nombreController.text;
                   final String apellido = _apellidoController.text;
                   final String user = _userController.text;
                   final String password = _passwordController.text;
 
-                  try {
-                    await crearUser(user, password, nombre, apellido);
-                    Navigator.popAndPushNamed(context, "/login");
-                  } catch (e) {
-                    print(e.toString());
+                  String? errorMsg;
+
+                  if (nombre.isEmpty && apellido.isEmpty && user.isEmpty && password.isEmpty) {
+                    errorMsg = 'Todos los campos están vacíos.';
+                  } else {
+                    if (nombre.isEmpty) {
+                      errorMsg = 'El campo nombre no puede estar vacío.';
+                    } else if (apellido.isEmpty) {
+                      errorMsg = 'El campo apellido no puede estar vacío.';
+                    } else if (user.isEmpty) {
+                      errorMsg = 'El campo usuario no puede estar vacío.';
+                    } else if (password.isEmpty) {
+                      errorMsg = 'El campo contraseña no puede estar vacío.';
+                    }
+                  }
+
+                  if (errorMsg != null) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(errorMsg),
+                    ));
+                  } else {
+                    crearUser(user, password, nombre, apellido).then((_) {
+                      QuickAlert.show(
+                        context: context,
+                        type: QuickAlertType.success,
+                        text: '¡Usuario registrado correctamente!',
+                        confirmBtnText: 'Aceptar',
+                        onConfirmBtnTap: () {
+                          Navigator.popAndPushNamed(context, "/login");
+                        },
+                      );
+                    }).catchError((e) {
+                      print(e.toString());
+                    });
                   }
                 },
               ),
+
             ],
           ),
         ),
