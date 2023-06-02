29100fe99728eaf9ccb51b9d3dcb3d48d7442bd1
alanbacont <almabarocioco@ittepic.edu.mx>
2023-06-01 23:00:55 -0700

    :P

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,31 +1,16 @@
-import 'dart:convert';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:mime/mime.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
-
-void main() {
-  initializeDateFormatting().then((_) => runApp(const MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) => const MaterialApp(
-        home: ChatPage(),
-      );
-}
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -219,19 +204,33 @@ class _ChatPageState extends State<ChatPage> {
       text: message.text,
     );
 
+    FirebaseFirestore.instance
+        .collection('chat_messages')
+        .doc('chat_id')
+        .collection('messages')
+        .doc(textMessage.id)
+        .set(textMessage.toJson());
+
     _addMessage(textMessage);
   }
 
   void _loadMessages() async {
-    final response = await rootBundle.loadString('assets/messages.json');
-    final messages = (jsonDecode(response) as List)
-        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-        .toList();
+    final messagesSnapshot = await FirebaseFirestore.instance
+        .collection('chat_messages')
+        .doc('chat_id')
+        .collection('messages')
+        .get();
+
+    final messages = messagesSnapshot.docs.map((doc) {
+      final data = doc.data();
+      return types.Message.fromJson(data);
+    }).toList();
 
     setState(() {
       _messages = messages;
     });
   }
+
 
   @override
   Widget build(BuildContext context) => Scaffold(
